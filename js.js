e37d3e32fdf199bab7e59ea32fbfaef07116d6f3
Gabriel Peixoto <118762641+BerukenKun@users.noreply.github.com>
2024-06-22 18:12:17 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+function changeColor(element) {
+    element.style.color = 'magenta'; // Altera a cor para magenta quando passar o mouse
+}
+
+function resetColor(element) {
+    element.style.color = ''; // Resetar para a cor padrão
+}
